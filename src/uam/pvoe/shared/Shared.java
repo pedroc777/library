41e076e08278460b7cc89013ceb477b590096b05
pedroc777 <73049669+pedroc777@users.noreply.github.com>
2021-12-22 20:56:40 -0600

    Add files via upload

---
+++ src/uam/pvoe/shared/Shared.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uam.pvoe.shared;
+
+
+/**
+ * This is the Shared class, which is used to with data through the other classes.
+ * @author pedro
+ */
+public class Shared {
+    
+    /**
+     * This attribute is used to share the user of an User object.
+     */
+    public static String login;
+    
+    /**
+     * This attribute is used to share the username of an User object.
+     */
+    public static String userName;
+   
+}
